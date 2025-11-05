c1aa038b031ab81ae866b041f30e24b560ccf803
jzrlza <parinvut12345@gmail.com>
2025-11-05 09:44:15 +0700

    .

--- one_nbtc_ui_backend/config/database.js
+++ one_nbtc_ui_backend/config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3307,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root', 
-  password: process.env.DB_PASSWORD || 'rootsqljz',
-  database: process.env.DB_NAME || 'one_nbtc'
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || ''
 };
 
 const getConnection = async () => {
