a781ca44e0be5f5224f3a9a109282bc27b934842
jzrlza <parinvut12345@gmail.com>
2026-03-04 11:47:58 +0700

    .

--- one_nbtc_ui_frontend/src/components/Navbar.jsx
+++ one_nbtc_ui_frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = ({ user, onLogout }) => {
   return (
     <nav className="navbar-public">
-      <div className="nav-brand-public">NBTC Learn and Grow Registration</div>
+      <div className="nav-brand-public">ONE-NBTC Registration</div>
       <div className="nav-user-public">
         กรอกข้อมูลเพื่อลงทะเบียน
       </div>
--- one_nbtc_ui_frontend/src/pages/Home.jsx
+++ one_nbtc_ui_frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 import Modal from '../components/Modal';
-import bannerImage from '../res/banner.jpg';
+//import bannerImage from '../res/banner.jpg';
 import { registerEnums } from '../utils/enum_config';
 
 const Home = ({ user, onLogout }) => {
@@ -494,9 +494,9 @@ const Home = ({ user, onLogout }) => {
       
       <main className="app-main app-home">
         <section className="health-section">
-          <img className="img-banner" src={bannerImage} 
+          <img className="img-banner" src={""} 
             alt={
-              `Register Now | กิจกรรม NBTC Learn and Grow | Uptrend, Level Up, Grow Together | by NBTC Academy`
+              `null image currently`
             } 
           />
         </section>
