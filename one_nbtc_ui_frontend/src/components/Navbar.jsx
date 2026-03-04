21b662339ee7f2078098480d3a58bd718fba86f7
jzrlza <parinvut12345@gmail.com>
2026-03-04 16:45:16 +0700

    idiot git

--- one_nbtc_ui_frontend/src/components/Navbar.jsx
+++ one_nbtc_ui_frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import bannerImage from '../res/LOGO.png';
 
 const Navbar = ({ user, onLogout }) => {
   return (
     <nav className="navbar-public">
-      <div className="nav-brand-public">ONE-NBTC Registration</div>
+      <div className="nav-brand-public">
+      <img className="img-banner" src={bannerImage} 
+            alt={
+              `null image currently`
+            } 
+          /></div>
       <div className="nav-user-public">
         กรอกข้อมูลเพื่อลงทะเบียน
       </div>
