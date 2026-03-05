5ad9e3b5f2493601daa5b6fc8badb1a91eb1596c
jzrlza <parinvut12345@gmail.com>
2026-03-05 11:48:27 +0700

    .

--- testdeploy_db.sh
+++ testdeploy_db.sh
@@ -8,15 +8,15 @@ mysql -h 127.0.0.1 -P 3306 -u root -p
 # Password: from your .env file (MYSQL_ROOT_PASSWORD)
 
 # Or use Docker exec
-docker exec -it one_nbtc_ui_mysql mysql -u root -p
-#nbtc2026_lng
+docker exec -it one_nbtc_ui_database_production mysql -u root -p
+#nbtc2026_on
 # Check databases
 SHOW DATABASES;
-
+USE onenbtc_db;
 
 
 docker-compose stop database
 docker-compose rm -f database
 
 
-cat onenbtc.sql | docker exec -i one_nbtc_ui_mysql mysql -u root -p
+cat onenbtc.sql | docker exec -i one_nbtc_ui_database_production mysql -u root -p
